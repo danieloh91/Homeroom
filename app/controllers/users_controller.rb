2f858c612b8b3a8e320fb3386ad3d31f87506f62
danieloh91 <daniel.oh.91@gmail.com>
2016-09-10 15:44:38 -0700

    adds create method to user controller and changes to forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,25 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    set_user
+    if @user.save
+      login(@user)
+      flash[:notice] = "Your account was successfully created! Welcome to HomeRoom!"
+      redirect_to @user
+    else
+      flash[:notice] = "There was a problem creating your account. #{@user.errors.full_messages.join(', ')}."
+      redirect_to new_user_path
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :image, :role, :course)
+  end
+
+  def set_user
+    @user = User.new(user_params)
+  end
 end
