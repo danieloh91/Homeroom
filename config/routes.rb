06039c548b611014b72e5c2d40d843fa8153d735
danieloh91 <daniel.oh.91@gmail.com>
2016-09-10 16:11:53 -0700

    adds sessions controller and routes to sign up and log in on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   patch "/users/:id", to: "users#update"
   delete "/users/:id", to: "users#destroy", as: "destroy_user"
 
+  get "/login", to: "sessions#new", as: "new_session"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create", as: "create_session"
 end
