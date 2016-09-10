688fb3b1c135c53c9bd0756ab71b8151fea6d0c5
danieloh91 <daniel.oh.91@gmail.com>
2016-09-10 14:02:12 -0700

    adds routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: "users#splash"
+
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy", as: "destroy_user"
+
 end
