18bf5dc36327f8392662b4560a13cd2898a6c7ac
danieloh91 <daniel.oh.91@gmail.com>
2016-09-10 21:31:29 -0700

    saves role boolean and course info

--- app/assets/javascripts/users.js
+++
@@ -1,7 +0,0 @@
-var change = function() {
-   $(".radio").on("change", function(){
-     var id = $(this).data('id');
-     $('#' + id).addClass('none').siblings().removeClass('none');
-   });
-};
-$(document).on("page:load ready", change);
